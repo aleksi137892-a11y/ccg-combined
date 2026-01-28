275a33362fc2275d185b4e44992529fd5cdac1ce
aleksi137892-a11y <aleksi137892@gmail.com>
2026-01-28 01:47:09 +0100

    Remove duplicate nested supabase folder

--- supabase/supabase/migrations/20260120162455_9d62c6ca-c916-4d62-8ffb-2b7b1d76ac27.sql
+++
@@ -1,6 +0,0 @@
--- Add UPDATE policy for complicity_entities to allow upserts
-CREATE POLICY "Allow anonymous updates for data upload"
-ON public.complicity_entities
-FOR UPDATE
-USING (true)
-WITH CHECK (true);
